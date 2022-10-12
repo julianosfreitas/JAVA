f918fbff09cd4588942677c3a015c68c20cdcac7
Juliano Freitas <julianosfreitas@yahoo.com>
2022-10-11 23:34:30 -0300

    Add files via upload

---
+++ Area com PI e Raio/area.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class area{
+    public static void main(String[] args){
+        Scanner robo = new Scanner(System.in);
+        System.out.print("Digite o valor do Raio: ");
+        int raio = robo.nextInt();
+        System.out.print("O Raio é de : " + raio);
+    }
+}
